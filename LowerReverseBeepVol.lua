068d8b9e3221c140acaa514f312e48cb89446da5
Sebastian <ss@kvisa.de>
2022-12-23 01:01:22 +0100

    v 0.9.1.1

--- LowerReverseBeepVol.lua
+++ LowerReverseBeepVol.lua
@@ -17,31 +17,17 @@ local RDver = "0.9.0.0 beta"
 
  print("load script LRBV FS22_LowerReverseBeepVol version " .. RDver .. " (by SbSh) <--"); -- self.version
 
-function LowerReverseBeepVol:loadSounds(xmlFile, baseString)
-    if self.isClient then
-        local spec = self.spec_motorized
+function LowerReverseBeepVol:onUpdate(dt, isActiveForInput, isActiveForInputIgnoreSelection, isSelected)
+    local spec = self.spec_motorized
 
-        if spec.samples.reverseDrive ~= nil then
-            spec.samples.reverseDrive = nil -- drop sample
-			--setSampleVolume(spec.samples.reverseDrive, 0.2)
-			print("LRBV ------------- it goes ----------------")
-			print ("LRBV  Volume= " .. RDSvol .." factor ")
-        else
-		
-             --LowerReverseBeepVol:PrintDebug("LRBV sample name: " .. sample.sampleName);
-             --LowerReverseBeepVol:PrintDebug("LRBV spec  name : " .. spec.samples.reverseDrive);
-             print("LRBV --> FAIL: spec.samples.reverseDrive is nil = " .. RDSvol);
-			 
-             print("LRBV --> FAIL: nil content overritten by engine");
-			 
-			--setSampleVolume(spec.samples.reverseDrive.volumeScale, 0.3) --doesn't work -> samples.reverseDrive = nil index
-			-- setSampleVolume(spec.samples.reverseDrive, 0.4 * self:getCurrentSampleVolume(sample)) -- no sample
-			--print("LRBV Current vol: " ..self:getCurrentSampleVolume(sample))  -- nil
-            spec.samples.reverseDrive = nil -- dropping works = no beepsound
-			print("LRBV beep sound dropped")
+    if self:getIsMotorStarted() then
+        if self.isClient then
+            local samples = spec.samples
+			if samples.reverseDrive ~= nil then
+				g_soundManager:setSampleVolumeOffset(samples.reverseDrive, 4)
+			end
         end
-        
     end
 end
 
-Motorized.loadSounds = Utils.appendedFunction(Motorized.loadSounds, LowerReverseBeepVol.loadSounds)
+Motorized.onUpdate = Utils.appendedFunction(Motorized.onUpdate, LowerReverseBeepVol.onUpdate)
