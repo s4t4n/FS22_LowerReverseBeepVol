74b2b50418f6508488733ccaf4e513df165289b3
Sebastian <ss@kvisa.de>
2022-12-25 20:16:44 +0100

    0.9.1.1 push to 1.0.0.0 for modHub

--- LowerReverseBeepVol.lua
+++ LowerReverseBeepVol.lua
@@ -4,18 +4,18 @@ LowerReverseBeepVol = {}
 -- save xml for mp/sp the volume in mp for each player serverside or clientside ?!
 -- 
 --
---
---
--- v 0.9.0.1 beta
--- SbSh (s4t4n)
--- 30.11.2022
+-- v 0.9.1.1
+-- SbSh / frvetz
+-- 30.11.2022 - 0.9..
+-- 22.12.2022 - 0.9.1.1 md:1.0.0.0
 -- 
 
 
-local RDSvol = 0.2;  -- volume scale 0.4 means 60 %
-local RDver = "0.9.0.0 beta"
+local RDSvol = 0.4  -- volume scale 0.4 means 60 %
+local RDver = "0.9.1.1"
+local RDauthor = "SbSh / frvetz"
 
- print("load script LRBV FS22_LowerReverseBeepVol version " .. RDver .. " (by SbSh) <--"); -- self.version
+ print("load script LRBV FS22_LowerReverseBeepVol version " .. RDver .. " ".. RDauthor .." <--") -- self.version
 
 function LowerReverseBeepVol:onUpdate(dt, isActiveForInput, isActiveForInputIgnoreSelection, isSelected)
     local spec = self.spec_motorized
@@ -24,10 +24,11 @@ function LowerReverseBeepVol:onUpdate(dt, isActiveForInput, isActiveForInputIgno
         if self.isClient then
             local samples = spec.samples
 			if samples.reverseDrive ~= nil then
-				g_soundManager:setSampleVolumeOffset(samples.reverseDrive, 4)
+				g_soundManager:setSampleVolumeOffset(samples.reverseDrive, RDSvol)
 			end
         end
     end
 end
 
 Motorized.onUpdate = Utils.appendedFunction(Motorized.onUpdate, LowerReverseBeepVol.onUpdate)
+
